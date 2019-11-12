2224ceac93c716e1d6cce8c4482469491aef2fd3
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2019-11-12 01:33:53 +0100

    Clean

--- Sources/SwiftBuildSystemGeneratorKit/Helpers/Optional+Unwrap.swift
+++ Sources/SwiftBuildSystemGeneratorKit/Helpers/Optional+Unwrap.swift
@@ -1,6 +1,5 @@
-// From: https://ericasadun.com/2016/10/07/converting-optionals-to-thrown-errors/
-
 extension Optional {
+    /// From: https://ericasadun.com/2016/10/07/converting-optionals-to-thrown-errors/
     public func unwrap(onFailure description: String, file: String = #file, line: Int = #line) throws -> Wrapped {
         guard let unwrapped = self else {
             throw UnexpectedError(description, file: file, line: line)
