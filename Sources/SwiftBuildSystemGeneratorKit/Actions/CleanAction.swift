88c3da457b2d620a21ef1d27fc70760fa9a78f82
Andrés Cecilia Luque <andres.luque@cellpointmobile.com>
2019-11-27 17:48:30 +0100

    Clean

--- Sources/SwiftBuildSystemGeneratorKit/Actions/CleanAction.swift
+++ Sources/SwiftBuildSystemGeneratorKit/Actions/CleanAction.swift
@@ -9,29 +9,29 @@ public class CleanAction: Action {
     public func execute() throws {
         Reporter.print("Removing existing configuration files from path: \(Current.wd)")
 
-        let modules = try FileIterator().start()
-
-        Reporter.print("Found modules:")
-        modules.forEach {
-            let relativePath = $0.path.relative(to: Current.wd)
-            Reporter.print("\(relativePath)")
-        }
-
         let converters: [ConverterInterface] = Current.options.converters.map {
             $0.build()
         }
 
-        if converters.isEmpty == false {
-            for converter in converters {
-                try converter.clean()
+        if converters.isEmpty {
+            let modules = try FileIterator().start()
+
+            Reporter.print("Found modules:")
+            modules.forEach {
+                let relativePath = $0.path.relative(to: Current.wd)
+                Reporter.print("\(relativePath)")
             }
-        } else {
+
             let generators: [GeneratorInterface] = Current.options.generators.map {
                 $0.build(modules)
             }
 
             for generator in generators {
                 try generator.clean()
+            }
+        } else {
+            for converter in converters {
+                try converter.clean()
             }
         }
 
