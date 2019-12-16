4282a2a630f17f70b79dfd24b511e9f66bd333f8
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2019-12-16 01:12:27 +0100

    Added autoInit template

--- Sources/SwiftBuildSystemGeneratorKit/Entities/Global.swift
+++ Sources/SwiftBuildSystemGeneratorKit/Entities/Global.swift
@@ -1,8 +1,10 @@
 import Foundation
+import Version
+import Path
 
 public struct Global: Codable, AutoDecodable, DictionaryConvertible {
-    public static let defaultSupportPath = "SupportingFiles"
     public static let defaultFolderStructure: FolderStructure = .gradle
+    public static let defaultSupportPath = "SupportingFiles"
     public static let defaultIgnore: [Path] = []
 
     public let version: Version
--- Sources/SwiftBuildSystemGeneratorKit/Entities/Options.swift
+++ Sources/SwiftBuildSystemGeneratorKit/Entities/Options.swift
@@ -35,6 +35,7 @@ public struct Options {
 }
 
 extension Options {
+    // sourcery: AutoInit
     public struct Yaml: Codable {
         public let fileName: String?
         public let templatePath: String?
@@ -43,24 +44,6 @@ extension Options {
         public let generators: [Generator]?
         public let converters: [Converter]?
         public let carthagePath: Path?
-
-        public init(
-            fileName: String?,
-            templatePath: String?,
-            generateXcodeProject: Bool?,
-            generateXcodeWorkspace: Bool?,
-            generators: [Generator]?,
-            converters: [Converter]?,
-            carthagePath: Path?
-        ) {
-            self.fileName = fileName
-            self.templatePath = templatePath
-            self.generateXcodeProject = generateXcodeProject
-            self.generateXcodeWorkspace = generateXcodeWorkspace
-            self.generators = generators
-            self.converters = converters
-            self.carthagePath = carthagePath
-        }
 
         public func merge(with yaml: Yaml?) -> Yaml {
             return Yaml(
@@ -73,5 +56,17 @@ extension Options {
                 carthagePath: carthagePath ?? yaml?.carthagePath
             )
         }
+
+// sourcery:inline:auto:Options.Yaml.AutoInit
+        public init(fileName: String?, templatePath: String?, generateXcodeProject: Bool?, generateXcodeWorkspace: Bool?, generators: [Generator]?, converters: [Converter]?, carthagePath: Path?) { // swiftlint:disable:this line_length
+            self.fileName = fileName
+            self.templatePath = templatePath
+            self.generateXcodeProject = generateXcodeProject
+            self.generateXcodeWorkspace = generateXcodeWorkspace
+            self.generators = generators
+            self.converters = converters
+            self.carthagePath = carthagePath
+        }
+// sourcery:end
     }
 }
