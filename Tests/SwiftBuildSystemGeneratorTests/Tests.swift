9bbf483f89b0697ab528bbc24aabfb6664e94acc
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2020-03-12 02:21:17 +0100

    Added template to create a top level project when using XcodeGen

--- Tests/SwiftBuildSystemGeneratorTests/Tests.swift
+++ Tests/SwiftBuildSystemGeneratorTests/Tests.swift
@@ -7,19 +7,35 @@ final class Tests: XCTestCase {
     func testGenerate() throws {
         for templatesPath in templatesPath.ls().directories {
             let result = try generate(templatesPath)
-            XCTAssertEqual(result.status, 0)
+            XCTAssertEqual(result.exitCode, 0)
             assert(fixture: fixturesPath/templatesPath.basename(), equals: result.destination)
         }
     }
 
-    private func generate(_ templatesPath: Path, function: String = #function) throws -> (destination: Path, status: Int32) {
+    func testXcodegen() throws {
+        let result = try generate(templatesPath/"xcodegen")
+        XCTAssertEqual(result.exitCode, 0)
+
+        // Generate Xcode projects
+        let scriptOutput = runCommand(cmd: "/bin/sh", args: "--login", "\(rootPath)/scripts/create-projects.sh", "\(result.destination)")
+        XCTAssertEqual(scriptOutput.exitCode, 0, scriptOutput.error.joined(separator: "\n"))
+
+        // Run unit tests
+        let xcodebuildOutput = runCommand(cmd: "/usr/bin/xcodebuild", args: "test", "-project", "\(result.destination)/All.xcodeproj", "-scheme", "All", "-destination", "platform=iOS Simulator,name=iPhone 8,OS=latest")
+        XCTAssertEqual(xcodebuildOutput.exitCode, 0, xcodebuildOutput.error.joined(separator: "\n"))
+        if xcodebuildOutput.exitCode != 0 {
+            runCommand(cmd: "/usr/bin/open", args: "\(result.destination)/All.xcodeproj")
+        }
+    }
+
+    private func generate(_ templatesPath: Path, function: String = #function) throws -> (destination: Path, exitCode: Int32) {
         let destination = try examplesPath.copy(into: try tmp(testName: function))
         FileManager.default.changeCurrentDirectoryPath(destination.string)
         try patchWorkspaceFile(destination, using: templatesPath)
 
         let cli = SwiftBuildSystemGeneratorCLI()
-        let status = cli.execute(with: generateCommandArgs())
+        let exitCode = cli.execute(with: generateCommandArgs())
 
-        return (destination: destination, status: status)
+        return (destination: destination, exitCode: exitCode)
     }
 }
